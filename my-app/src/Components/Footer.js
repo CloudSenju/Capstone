51dd42a31170a8552eaa158273074a83049239e6
Willem Claassens <willemc@xlink.co.za>
2023-10-05 14:31:16 +0200

    Committer: Willem Claassens <willemc@xlink.co.za>

---
+++ my-app/src/Components/Footer.js
@@ -0,0 +1,43 @@
+
+
+function Footer() {
+    return (
+      <>
+      <footer>
+          <div class="row">
+            <div class="column"> Logo</div>
+            <div class="column">
+              <ul id="footer-list">
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+              </ul>
+            </div>
+            <div class="column"></div>
+              <ul id="footer-list">
+                <li class="heading">Contact Us</li>
+                <br/>
+                <li>Address</li>
+                <li>Email</li>
+                <li>Telephone Number</li>
+              </ul>
+            <div></div>
+            <div class="column">
+              <ul id="footer-list">
+                <li class="heading">Social Media</li>
+                <br/>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>X</li>
+              </ul>
+            </div>
+         </div>
+        </footer>
+      </>
+    );
+  }
+  
+  export default Footer;
---
+++ my-app/src/Components/Nav.js
@@ -0,0 +1,15 @@
+function Nav() {
+    return (
+      <>
+        <nav>
+          {/* <img href="" alt="logo"></img> */}
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Reservation</a>
+          <a href="">Order Online</a>
+        </nav>
+      </>
+    );
+  }
+  
+  export default Nav;
