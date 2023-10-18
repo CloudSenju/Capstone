2106999e9c53507e46614d999524cf423753d330
Willem Claassens <willemc@xlink.co.za>
2023-10-18 15:18:44 +0200

    styling0.1

--- my-app/src/Components/Main.js
+++ my-app/src/Components/Main.js
@@ -2,15 +2,18 @@ import Hero from './Main Components/Hero';
 import Highlights from './Main Components/Highlights';
 import Testimonials from './Main Components/Testimonials';
 import About from './Main Components/About';
+import greekSalad from '../Assets/greek salad.jpg';
+import bruchetta from '../Assets/bruchetta.svg';
+import lemonDessert from '../Assets/lemon dessert.jpg';
 
 function Main() {
     return (
       <>
         <Hero/>
         <div id="highlights-container">
-          <Highlights dish="Greek Salad" price="$12,99" image="..\Assets\greek salad.jpg" description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
-          <Highlights dish="Greek Salad" price="$12,99" image="https://peasandcrayons.com/wp-content/uploads/2021/05/marinated-greek-salad-recipe-2-500x500.jpg" description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
-          <Highlights dish="Greek Salad" price="$12,99" image="https://peasandcrayons.com/wp-content/uploads/2021/05/marinated-greek-salad-recipe-2-500x500.jpg" description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
+          <Highlights dish="Greek Salad" price="$12,99" image={greekSalad} description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
+          <Highlights dish="Bruchetta" price="$10,99" image={bruchetta} description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
+          <Highlights dish="Lemon Dessert" price="$14,99" image={lemonDessert} description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
           <Highlights dish="Greek Salad" price="$12,99" image="https://peasandcrayons.com/wp-content/uploads/2021/05/marinated-greek-salad-recipe-2-500x500.jpg" description="A traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese. This classic combination is delicious, so I stick to it, just adding a handful of mint leaves for a fresh finishing touch."/>
         </div>
         <div id="testimonials-container">
